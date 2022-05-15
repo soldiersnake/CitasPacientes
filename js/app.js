34a59db49a06566193aaaeb8757c873e4ba446a4
soldiersnake <mariano.maciasgandulfo@gmail.com>
2022-05-15 20:02:55 +0200

    mensaje de alerta agregado al DOM

--- js/app.js
+++ js/app.js
@@ -10,6 +10,42 @@ const sintomasInput = document.querySelector('#sintomas');
 const formulario = document.querySelector('#nueva-cita');
 const contenedorCitas = document.querySelector('#citas');
 
+class Citas {
+    constructor(){
+        this.citas = [];
+    }
+
+};
+
+class Ui {
+    imprimirAlerta(mensaje, tipo){
+        // Crear el div
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('text-center', 'alert', 'd-block', 'col-12');
+
+        // Agregar clase en base al tipo de error
+        if(tipo === 'error'){
+            divMensaje.classList.add('alert-danger');
+        }else{
+            divMensaje.classList.add('alert-success');
+        };
+
+        // mensaje de error
+        divMensaje.textContent = mensaje;
+
+        //agregar al DOM
+        document.querySelector('#contenido').insertBefore(divMensaje, document.querySelector('.agregar-cita'));
+
+        //quitar el mensaje
+        setTimeout(() => {
+            divMensaje.remove();
+        }, 3000);
+    }
+};
+
+const ui = new Ui();
+const administrarCitas = new Citas();
+
 // registrar eventos
 eventListeners();
 function eventListeners(){
@@ -19,6 +55,8 @@ function eventListeners(){
     fechaInput.addEventListener('change', datosCita);
     horaInput.addEventListener('change', datosCita);
     sintomasInput.addEventListener('change', datosCita);
+
+    formulario.addEventListener('submit', nuevaCita);
 };
 
 //objeto con la info de cita
@@ -36,3 +74,17 @@ function datosCita(e){
     citaObj[e.target.name] = e.target.value;
     console.log(citaObj);
 };
+
+// valida y agrega una nueva cita a la calse de citas
+function nuevaCita(e){
+    e.preventDefault();
+
+    // extraer la informacion del objeto de cita
+    const { mascota, propietario, telefono, fecha, hora, sintomas } =citaObj;
+
+    // validar
+    if( mascota === '' || propietario === '' || telefono === '' || fecha === '' || hora === '' || sintomas === ''){
+        ui.imprimirAlerta('Todos los campos son obligatorios', 'error');
+        return;
+    };
+}
